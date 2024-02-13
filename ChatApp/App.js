6714c917e1f188b965c0b9347bdfef53994bf407
Guilherme Fernandes <guilhermelff@id.uff.br>
2024-02-13 20:02:51 -0300

    mock screen

--- ChatApp/App.js
+++ ChatApp/App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Chats from './Chats.js';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! Hello World</Text>
+    <View>
       <StatusBar style="auto" />
+      <Chats />
     </View>
   );
 }
